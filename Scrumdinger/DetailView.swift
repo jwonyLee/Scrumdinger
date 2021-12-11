acd1e1aa8229fbb548955e71446606864fe95a69
JiWon Lee <jiwon.swdev@gmail.com>
2021-12-11 15:13:46 +0900

    passing data with bindings

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
 
+    @State private var data = DailyScrum.Data()
     @State private var isPresentingEditView = false
 
     var body: some View {
@@ -37,7 +38,6 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
-
             Section(header: Text("Attendees")) {
                 ForEach(scrum.attendees) { attendee in
                     Label(attendee.name, systemImage: "person")
@@ -48,11 +48,12 @@ struct DetailView: View {
         .toolbar {
             Button("Edit") {
                 isPresentingEditView = true
+                data = scrum.data
             }
         }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationView {
-                DetailEditView()
+                DetailEditView(data: $data)
                     .navigationTitle(scrum.title)
                     .toolbar {
                         ToolbarItem(placement: .cancellationAction) {
@@ -63,6 +64,7 @@ struct DetailView: View {
                         ToolbarItem(placement: .confirmationAction) {
                             Button("Done") {
                                 isPresentingEditView = false
+                                scrum.update(from: data)
                             }
                         }
                     }
@@ -74,7 +76,7 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
